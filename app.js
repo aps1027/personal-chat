88cf7cd4dd7a5ef01e74f4ce7c82aebf5c0c0424
aungpyaesone <pyaesonefpfsi@gmail.com>
2023-01-06 21:48:22 +0630

    add: deploy config

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
 app.use("/api", require("./routes/api"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 server.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
